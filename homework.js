b9c658c4c4ff2d733179e80b727354a4ca4d8563
Le-Minh-Dat-11 <minhdat240211@gmail.com>
2023-08-12 09:32:34 +0700

    homework-3

---
+++ homework.js
@@ -0,0 +1,15 @@
+var blank = [["grape", 15], ["banana", 12], ["apple", 13]]
+// var i = blank.splice(1, 1);
+console.log(blank)
+// console.log("I have", i)
+var text = ""
+for (let i in blank) {
+    text += blank[i] + " "
+}
+console.log(text);
+var Husky = {
+    "name": "Ngáo",
+    "leg": "4",
+    "color": "brown"
+}
+console.log(Husky)
